9637261f887abde0193052eb607cf7ad46a93713
Godwin Onye <mediad@macbookpro.home>
2025-11-07 18:55:21 +0000

    Increase body size limit to 10MB for logo uploads

--- server.js
+++ server.js
@@ -165,7 +165,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' })); // Increase limit for logo uploads
 
 // Cookie parser for sessions
 const cookieParser = require('cookie-parser');
